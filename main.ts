7240596e533a42e6319910fe74d8cc9d61106569
kingmickey08 <81299672+kingmickey08@users.noreply.github.com>
2022-09-28 12:15:10 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    }
+})
